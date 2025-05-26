b16d7fabaafcb5b048e92631b57d6fd91fc8a0d8
Rafał Grzelak <rafikg523@gmail.com>
2025-05-26 22:17:04 +0200

    better

--- src/Lab10/Zad1.java
+++ src/Lab10/Zad1.java
@@ -17,44 +17,27 @@ public class Zad1 {
 
         Hamming ham = new Hamming(7,4);
 
-        int[][] encoded = ham.encoder(rng);
+        int[] data = new int[]{1,1,0,1};
 
+        int[] raw = rng;
 
-        for (int i = 0; i < encoded.length; i++) {
-            System.out.println(i + 1 + ". " + Arrays.toString(encoded[i]));
+        int[] encoded = ham.encoder(raw);
+        System.out.println("encoded: " + Arrays.toString(encoded));
 
+        encoded[2] ^= 1;
+        encoded[15] ^= 1;
+        encoded [30] ^= 1;
+
+        int[] decoded = ham.decoder(encoded);
+
+        System.out.println("raw:     " + Arrays.toString(raw));
+        System.out.println("decoded: " + Arrays.toString(decoded));
+
+        if (Arrays.equals(raw, decoded)) {
+            System.out.println("Tablice takie same");
+        } else {
+            System.out.println("Tablice roznia sie");
         }
-        System.out.println();
-
-        int totalLength = encoded.length * encoded[0].length;
-        int[] flattened = new int[totalLength];
-        int index = 0;
-
-        for (int i = 0; i < encoded.length; i++) {
-            for (int j = 0; j < encoded[i].length; j++) {
-                flattened[index++] = encoded[i][j];
-            }
-        }
-
-        Hamming ham2 = new Hamming(7,4);
-
-        flattened[23] = flattened[23] == 1 ? 0 : 1;
-        //flattened[24] = flattened[24] == 1 ? 0 : 1;
-
-        int[][] decoded = ham2.decoder(flattened);
-
-        int totalLength2 = decoded.length * decoded[0].length;
-        int[] flattened2 = new int[totalLength2];
-        int index2 = 0;
-
-        for (int i = 0; i < decoded.length; i++) {
-            for (int j = 0; j < decoded[i].length; j++) {
-                flattened2[index2++] = decoded[i][j];
-            }
-        }
-
-        System.out.println(Arrays.toString(rng));
-        System.out.println(Arrays.toString(flattened2));
 
     }
 
