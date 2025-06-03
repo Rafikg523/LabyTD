7c576f8d9e659533261c81e3753af99df69aa08c
Rafał Grzelak <rafikg523@gmail.com>
2025-06-03 11:48:32 +0200

    lab11 git

--- src/Lab11/Zad1.java
+++ src/Lab11/Zad1.java
@@ -1,40 +1,38 @@
-package Lab10;
+package Lab11;
 
-
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
 public class Zad1 {
-
     public static void main(String[] args) {
-        int n = 50;
+        int n = 70;
         Random random = new Random();
         int[] rng = new int[n];
         for (int i = 0; i < n; i++) {
             rng[i] = random.nextInt(2);
         }
 
-        Hamming ham = new Hamming(7,4);
+        Hamming ham = new Hamming(15,11);
 
-        int[] data = new int[]{1,1,0,1};
+        int[] data = new int[]{1,1,0,1,1,0,0,1,1,0,0};
 
         int[] raw = rng;
 
+        System.out.println("Encoding:");
         int[] encoded = ham.encoder(raw);
-        System.out.println("encoded: " + Arrays.toString(encoded));
 
         encoded[2] ^= 1;
         encoded[15] ^= 1;
         encoded [30] ^= 1;
 
+        System.out.println("Decoding:");
         int[] decoded = ham.decoder(encoded);
 
+        System.out.println("Wynik:");
         System.out.println("raw:     " + Arrays.toString(raw));
         System.out.println("decoded: " + Arrays.toString(decoded));
 
         boolean git = true;
-
         for (int i = 0; i < raw.length; i++) {
             if(raw[i] != decoded[i]) {
                 git = false;
@@ -47,8 +45,5 @@ public class Zad1 {
         } else {
             System.out.println("Tablice roznia sie");
         }
-
     }
-
-
 }
